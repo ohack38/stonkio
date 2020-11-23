145b732b23d5f56448d7fdab8ca66359b350c6cb
Oscar Hackman <ohackman1@gmail.com>
2020-11-23 18:59:44 +0200

    init

---
+++ stonkapi/authentication/serializers.py
@@ -0,0 +1,68 @@
+from rest_framework import serializers
+from django.contrib import auth
+from rest_framework.exceptions import AuthenticationFailed
+
+from authentication.models import User
+
+class RegisterSerializer(serializers.ModelSerializer):
+
+    password = serializers.CharField(max_length=68, min_length=6, write_only=True)
+    
+    class Meta:
+        model = User
+        fields = ['email', 'username', 'password']
+
+    def validate(self,attrs):
+        email = attrs.get('email', '')
+        username = attrs.get('username','')
+
+        if not username.isalnum():
+            raise serializers.ValidationError('The username should contain only alphanumerics')
+        return attrs
+
+    def create(self, validated_data):
+        return User.objects.create_user(**validated_data)
+
+
+class EmailVerificationSerializer(serializers.ModelSerializer):
+    token = serializers.CharField(max_length=555)
+
+    class Meta:
+        model = User
+        fields = ['token']
+
+
+class LoginSerializer(serializers.ModelSerializer):
+    email = serializers.EmailField(max_length=255, min_length=3)
+    password = serializers.CharField(max_length=68, min_length=6, write_only=True)
+    username = serializers.CharField(read_only=True)
+    tokens = serializers.CharField(read_only=True)
+
+    class Meta:
+        model = User
+        fields = ['email', 'password', 'username', 'tokens']
+
+    def validate(self,attrs):
+        email = attrs.get('email', '')
+        password = attrs.get('password', '')
+
+        user = auth.authenticate(email=email, password=password)
+
+        if not user:
+            raise AuthenticationFailed('Invalid credentials, try again')
+        if not user.is_active:
+            raise AuthenticationFailed('Account disabled, contact admin')
+        
+        if not user.is_verified:
+            raise AuthenticationFailed('Email not verified')
+
+        
+
+        return{
+            'email': user.email,
+            'username': user.username,
+            'tokens': user.tokens
+        }
+
+        
+        return super().validate(attrs)
---
+++ stonkapi/authentication/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+from .views import RegisterView,VerifyEmail, LoginAPIView
+
+
+urlpatterns = [
+    path('register/', RegisterView.as_view(), name='register'),
+    path('login/', LoginAPIView.as_view(), name='login'),
+    path('verify-email/', VerifyEmail.as_view(), name='verify-email'),
+]
