7d50fea5349ff691a868f847d6acfdad4113ed52
Oscar Hackman <ohackman1@gmail.com>
2021-01-19 23:11:58 +0200

    react setup & sidebar ui

---
+++ client/src/components/Sidebar/MenuItemComponent.js
@@ -0,0 +1,25 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+import { Row } from 'simple-flexbox'
+
+const MenuItemComponent = (props) => {
+
+    const { active, icon, title } = props
+    const Icon = icon 
+
+    return(
+        <Row className={ active ? 'activeContainer' : 'menuContainer'} vertical='center'>
+            
+            <Icon fill={active && "#DDE2FF"} opacity={!active && "0.4"} />
+            <span className={ active ? 'activeTitle' : 'menuTitle' }>{title}</span>
+        </Row>
+    )
+}
+
+MenuItemComponent.propTypes = {
+    active: PropTypes.bool,
+    icon: PropTypes.func,
+    title: PropTypes.string
+}
+
+export default MenuItemComponent
