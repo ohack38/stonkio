8eedc532ec648620ee6a1b5a4d90bc5464a416c2
Oscar Hackman <ohackman1@gmail.com>
2021-01-21 11:24:22 +0200

    main components

---
+++ client/src/components/Prices/PriceComponent.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const PriceComponent = () => {
+
+    //map prices, for item <PriceItem />
+    return (
+        <div>
+            <h1>Prices coming soon..</h1>
+            
+        </div>
+    );
+}
+
+export default PriceComponent;
---
+++ client/src/components/Watchlist/WatchlistComponent.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+
+const WatchlistComponent = () => {
+    return (
+        <div>
+            <h1>Watchlist</h1>
+        </div>
+    );
+}
+
+export default WatchlistComponent;
